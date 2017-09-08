f471a94e86465d7e7b89d6e3f8ffc0e31d4d2be4
Michael Daines <michaeld@healthfinch.com>
2017-09-08 14:03:41 -0700

    Upgrades to Clojure 1.9 alpha 20

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/defndaines/gilded-rose"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
                  [com.cerner/clara-rules "0.15.2"]])
